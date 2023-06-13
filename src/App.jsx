cf3b46e3d2cac14c43b227f5a78e11c88346cde0
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-06-13 13:39:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-red-400 h-screen">
+        <h1 className="text-3xl font-semibold text-center py-10">
+          React Redux Toolkit
+        </h1>
+      </div>
+    </>
+  );
+}
