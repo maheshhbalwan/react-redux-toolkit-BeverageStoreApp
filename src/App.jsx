2a41de3497fd91a67b5bfffde4f3fbd3ef03a65a
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-06-13 14:18:24 +0530

    add: product cards and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-export default function App() {
+import products from "../src/db/products.json";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+
+function App() {
   return (
-    <>
-      <div className="bg-red-400 h-screen">
-        <h1 className="text-3xl font-semibold text-center py-10">
-          React Redux Toolkit
-        </h1>
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <div class="flex mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <Cart />
       </div>
-    </>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <Product {...product} />
+        ))}
+      </div>
+    </div>
   );
 }
+
+export default App;
---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="text-center bg-red-400 w-full p-5">
+      <h1 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+        Total Items: 500 (Rs. 2000)/-
+      </h1>
+    </div>
+  );
+};
+
+export default Cart;
