3405e0c9f742797eae2017c7bb93db6ae0bb309e
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-07-03 23:35:49 +0530

    deploy with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-redux-toolkit-BeverageStoreApp/ ",
   server: {
     port: 3000,
   },
